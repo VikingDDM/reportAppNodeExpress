0f7bff784d80b0975ad8f658a567a4398941b929
TalentDevMan <kirril.dev.talent@gmail.com>
2023-02-28 12:35:12 +0100

    I built several apis for connecting frontend and backend

--- app/controllers/report.controller.js
+++ app/controllers/report.controller.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+const mongodb = require('mongodb');
 
 const Report = db.report;
 const { report } = require("../models");
@@ -32,3 +33,43 @@ exports.getreporting = (req, res) => {
         res.status(200).send(reports);
     });
 };
+
+exports.deletereporting = (req, res) => {
+    const userid = req.params._id;
+    Report.deleteOne({_id: new mongodb.ObjectID(userid)}, function(err, results) {
+        if (err){
+            res.status(401).send({message: err});
+            throw err;
+        }
+        res.status(200).send({message: "Delete successful!"}); 
+     });
+}
+
+exports.updatereporting = (req, res) => {
+    const userid = req.body._id;
+    const updatedReport = {
+        content: req.body.content,
+        updatedAt: req.body.updatedAt
+    };
+    
+
+    Report.findByIdAndUpdate(userid, updatedReport, { new: true }, function(
+        err,
+        updatedReport
+      ) {
+        if (err) {
+          res.status(500).send({message: err});
+          throw err;
+        } 
+          console.log("success");
+          res.status(200).send({message: "Update successful!"});
+      });
+}
+
+exports.getallreporting = (req, res) => {
+    Report.find({}, function(err, reports) {
+        res.status(200).send(reports);
+
+    });
+    
+}
--- app/routes/report.routes.js
+++ app/routes/report.routes.js
@@ -3,4 +3,7 @@ const controller = require("../controllers/report.controller");
 module.exports = function (app) {
     app.post("/api/report/createreporting", controller.createreporting);
     app.get("/api/report/getreporting/:name", controller.getreporting);
+    app.delete("/api/report/deletereporting/:_id", controller.deletereporting);
+    app.post("/api/report/updatereporting", controller.updatereporting);
+    app.get("/api/report/getallreporting", controller.getallreporting)
 };
